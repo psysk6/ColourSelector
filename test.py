d34564caf3399ee077106a2d5cdae20d69350f39
Samuel King <psysk6@nottingham.ac.uk>
2020-07-01 03:11:29 +0100

    added some click and colour selection tests!

---
+++ test.py
@@ -0,0 +1,22 @@
+from pynput.mouse import Listener
+from PIL import ImageGrab
+
+def get_pixel_colour(i_x, i_y):
+    try:
+        out = ImageGrab.grab().load()[i_x, i_y]
+    except IndexError:
+        print("error wow that's a big number!")
+    else:
+        return out
+
+def on_move(x,y):
+    pass
+def on_click(x,y,button,pressed):
+    output = ("click at {0},{1},{2},{3}").format(x,y,button,pressed)
+    print(get_pixel_colour(x,y))
+    print(output)
+
+
+with Listener(on_click=on_click) as Listener:
+    Listener.join()
+
