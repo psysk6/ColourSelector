20e3a63eea828c9424ec2a84fcf56f03aff54044
Samuel King <psysk6@nottingham.ac.uk>
2020-07-01 23:36:33 +0100

    pushed text, disabled resizing and set default size

--- newprojectapp.py
+++ newprojectapp.py
@@ -34,7 +34,7 @@ def on_click(x,y,button,pressed):
         print(get_pixel_colour(x,y))
         print(output)
     hexColour = convert_to_hex(get_pixel_colour(x,y))
-    widget.setColour(hexColour)
+    widget.setColour(hexColour,get_pixel_colour(x,y))
 
     
 
@@ -44,38 +44,45 @@ class Interface(tk.Frame):
     def __init__(self, master=None, **kw):
         tk.Frame.__init__(self, master, **kw)
         ViewOnline = tk.Button(self)
-        ViewOnline.config(justify='left', takefocus=True, text='View Online')
+        ViewOnline.config(justify='left', takefocus=True, text='Select Colour')
         ViewOnline.place(anchor='nw', x='60', y='160')
-        rgbText = tk.Text(self)
-        rgbText.config(height='1', insertunfocussed='none', setgrid='false', width='15')
+        self.rgbText = tk.Text(self)
+        self.rgbText.config(height='1', insertunfocussed='none', setgrid='false', width='15')
         _text_ = '''hex'''
-        rgbText.insert('0.0', _text_)
-        rgbText.place(anchor='nw', x='50', y='120')
+        self.rgbText.insert('0.0', _text_)
+        self.rgbText.place(anchor='nw', x='50', y='120')
         rgbLabel = tk.Label(self)
         rgbLabel.config(text='RGB:')
         rgbLabel.place(anchor='nw', x='10', y='120')
         hexLabel = tk.Label(self)
         hexLabel.config(text='Hex:')
         hexLabel.place(anchor='nw', x='10', y='90')
-        hexText = tk.Text(self)
-        hexText.config(height='1', setgrid='false', width='15')
+        self.hexText = tk.Text(self)
+        self.hexText.config(height='1', setgrid='false', width='15')
         _text_ = '''rgb
 '''
-        hexText.insert('0.0', _text_)
-        hexText.place(anchor='nw', x='50', y='90')
+        self.hexText.insert('0.0', _text_)
+        self.hexText.place(anchor='nw', x='50', y='90')
         self.frame_3 = tk.Frame(self)
         self.frame_3.config(background='#8680ff', height='70', width='70')
         self.frame_3.place(anchor='nw', x='70', y='5')
        
         
 
-    def setColour(self,hex):
-        self.frame_3.config(bg = hex)  
-
+    def setColour(self,hex,rgb):
+        self.frame_3.config(bg = hex) 
+       
+        self.hexText.delete(1.0,"end")
+        self.hexText.insert(1.0,hex)
+        
+        self.rgbText.delete(1.0,"end")
+        self.rgbText.insert(1.0,rgb)
 
 if __name__ == '__main__':
     root = tk.Tk()
     widget = Interface(root)
+    root.geometry("200x200")
+    root.resizable(0,0)
     widget.pack(expand=True, fill='both')
     listener = mouse.Listener(on_click=on_click)
     listener.start() # start thread
